e71fd4d1f4526da7a6cc33bf57ea39dc70f43eab
asingh20-tech <asingh20@oswego.edu>
2026-01-30 14:57:16 -0500

    changes

--- helloworld.c
+++ helloworld.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include <math.h>
+#include <ctype.h>
 
-int main(){
-    char name [25];
-    int age;
 
-    printf ("whats your name");
-    fgets(name,25,stdin);
+int main()
+{
+    char unit;
+    float temp;
 
-    printf("whats you age");
-    scanf("%d",&age);
-    printf("How much year %s old you are %d", name,age);
+    printf("\n is the temperature in (F)or (c)?: ");
+    scanf("%c",&unit);
+
+    unit = toupper(unit);
+
+    if(unit == 'c'){
+        printf("the temp is curre");
+
+    }
+    else if(unit == 'f'){
+
+    }
+
 
     return 0;
 }
