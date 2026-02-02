2d93f657a1ef3d02b14e97be229bbf0784e32bf9
asingh20-tech <asingh20@oswego.edu>
2026-02-01 19:40:52 -0500

    did all the basic commands

--- helloworld.c
+++ helloworld.c
@@ -1,20 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include <ctype.h>
-
-
-int findMax(int x , int y){
-    return (x<y)? x : y;
-}
-
+#include <stdlib.h>
 
 int main()
 {
-    //  trenary operator = shortcut to if/else when assigning/returning a value
-    // (condition) ? value if true :value if false
-    printf("%d\n",findMax(3,4));
+    char line[100];
+    char x[5], y[5], z[5];
+    fgets(line, sizeof(line), stdin);
+    int count = sscanf(line, "%4s %4s %4s", x, y, z);
+    int y1 = atoi(y);
+    int z1 = atoi(z);
+
+    if (count == 2 && strcmp(x,"col")==0){
+        printf("%d",y1);
+        while(1){
+            if (y1%2==0){
+                y1 = y1 /2;
+            }
+            else 
+            y1 = (3*y1)+1;
+        }
+        return 0;  
+    }
+        
+        
     
+    if (count == 3)
+    {
+        if (strcmp(x, "sum") == 0)
+        {
+            printf("%d", atoi(y) + atoi(z));
+        }
+        if (strcmp(x, "mul") == 0)
+        {
+            printf("%d", atoi(y) * atoi(z));
+        }
+        if (strcmp(x, "sub") == 0)
+        {
+            printf("%d", atoi(y) - atoi(z));
+        }
+        if (strcmp(x, "div") == 0)
+        {   if(strcmp(z,"0")==0){
+                printf("it is undefined");
+            }
+            else{
+                printf("%lf", atof(y)/atof(z));
+            }
 
-
+            
+        }
+        if (strcmp(x, "bye")==0){
+            printf("bye");
+        }
+    }
+    
     return 0;
 }
